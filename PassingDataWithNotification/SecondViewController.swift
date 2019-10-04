ef42da6c714b0f77bde4fa4d4639043d48f8533d
Ashish <aashishkc98@gmail.com>
2019-10-03 18:46:45 -0700

    Created UI

---
+++ PassingDataWithNotification/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PassingDataWithNotification
+//
+//  Created by Ashish on 10/3/19.
+//  Copyright © 2019 Greenhouse. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+
+}
