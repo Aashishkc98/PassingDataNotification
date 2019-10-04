2615d576af9c0abf337342adaac7666a31a05ef5
Ashish <aashishkc98@gmail.com>
2019-10-03 18:36:28 -0700

    PassingDataNotification

---
+++ PassingDataWithNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataWithNotification
+//
+//  Created by Ashish on 10/3/19.
+//  Copyright © 2019 Greenhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
